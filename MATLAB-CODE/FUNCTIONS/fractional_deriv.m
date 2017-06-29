c69202ac194bc17177ca0981d79d2451f2d8f323
Nicholas Mancinelli <n.j.mancinelli@gmail.com>
2017-06-29 10:10:51 -0400

    fractional deriv routine now accepts array agrgument

--- MATLAB-CODE/FUNCTIONS/fractional_deriv.m
+++ MATLAB-CODE/FUNCTIONS/fractional_deriv.m
@@ -1,9 +1,15 @@
-function [out]=fractional_deriv(a,n,t)
+function [out]=fractional_deriv(a,n,ts);
 
+out=ts*0.0;
+
+for ii =1:length(ts);
+t=ts(ii);cd 
+    
+    
 fun = @(w) 1/(2*pi)^0.5 * a*exp(-1/2 * a^2 * w.^2) .* exp(1i*w*t) .*(1i*w).^n;
 amp=integral(@(w)fun(w),-Inf,Inf);
 
-out=real(amp);
+out(ii)=real(amp);
 
 end
 
