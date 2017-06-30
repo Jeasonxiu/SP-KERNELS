98f8b73f52e736feba2d14e36dc58a932d3b6a65
Nicholas Mancinelli <n.j.mancinelli@gmail.com>
2017-06-30 12:08:52 -0400

    minor bug fix. was printing cd for no reason

--- MATLAB-CODE/FUNCTIONS/fractional_deriv.m
+++ MATLAB-CODE/FUNCTIONS/fractional_deriv.m
@@ -3,7 +3,7 @@ function [out]=fractional_deriv(a,n,ts);
 out=ts*0.0;
 
 for ii =1:length(ts);
-t=ts(ii);cd 
+t=ts(ii);
     
     
 fun = @(w) 1/(2*pi)^0.5 * a*exp(-1/2 * a^2 * w.^2) .* exp(1i*w*t) .*(1i*w).^n;
