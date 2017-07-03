1391a7506fd52f0fc6ba8201abbac6d3670b3bdf
Nicholas Mancinelli <n.j.mancinelli@gmail.com>
2017-07-03 13:04:47 -0400

    Figure 1 class looks good.

--- MATLAB-CODE/CLASSES/figure1.m
+++ MATLAB-CODE/CLASSES/figure1.m
@@ -1,26 +1,33 @@
 classdef figure1
 	%figure 1
-    properties
-        isochrons=zeros(50,50);
-    end
     
-    methods 
-        function plot(obj)
-            function add_labels()
+    methods
+        function obj=figure1()
+            
+            function add_decor()
                 ylabel('Depth (km)');
                 xlabel('Offset (km)');
+                set(gca,'YDir','reverse')
+                plot(0,0,'^r')
             end
             
-            figure(1); hold on;
-            subplot(2,1,1)
-            pcolor(obj.isochrons);
-            title('P-to-S');
-            add_labels();
+            rp=0.09*111.11;
             
-            subplot(2,1,2)
-            pcolor(obj.isochrons);
+            iso=isochrons(-600:15:150,0:2:300,rp);
+            
+            figure(1); 
+            subplot(2,1,1); hold on;
+            [C,h]=contour(iso.xs,iso.zs,iso.Ps,0:6:40,'black');
+            clabel(C,h,'FontWeight','bold','Color','blue')
+            title('P-to-S');
+            add_decor();
+  
+            
+            subplot(2,1,2); hold on;
+            [C,h]=contour(iso.xs,iso.zs,iso.Sp,-42:6:0,'black');
+            clabel(C,h,'FontWeight','bold','Color','blue')
             title('S-to-P');
-            add_labels();
+            add_decor();
             
         end
 
