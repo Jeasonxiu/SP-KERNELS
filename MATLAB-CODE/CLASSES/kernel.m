fd5a963c70ead3688694ab586f5b466b963cc24b
Nicholas Mancinelli <n.j.mancinelli@gmail.com>
2017-07-03 16:45:29 -0400

    Updated kernel class. Adding new method to look at amplitude factors.

--- MATLAB-CODE/CLASSES/kernel.m
+++ MATLAB-CODE/CLASSES/kernel.m
@@ -1,54 +1,63 @@
 classdef kernel
     properties
         Kernel
-        X
-        Y
-        Z
+        xs
+        zs
+        rps
         KTimes
         nTimes
         Angles
-        tDeci=1;
         Kernel_Type
+        model
+        X
+        Y
+        Z
+        tDeci=1;
         tchar=1.0
         nderiv=3.0
     end
     methods
         function obj=kernel(Kernel_Type)
             obj.Kernel_Type=Kernel_Type;
-            if Kernel_Type == 1;
-                path='/Volumes/nmancine/data2/nmancine/PROJECTS/SP_RECEIVER_FUNCTIONS/KERNEL/SP-KERNELS/DATA';
-                [Kernel,X,Y,Z,KTimes,nTimes,Angles] = load_kernel(path,obj.tDeci);
-            %elseif Kernel_Type ==2;
-            %    [Kernel,zs,xs,~,KTimes] = analytical_kernel_halfspace(26);
-            %    nTimes=length(KTimes);
-            %    [X,Y,Z]=meshgrid(xs,zs,1:nTimes);
-            %    TMP(:,:,:)=Kernel(:,:,1,:);
-            %    Kernel=TMP;
-            elseif Kernel_Type ==3;
-                [~,betatmp]=get_velocity_from_profile('MIGRA/myvmod.nd',300);
-                incangs=[20,23,26];
-                rps=sind(incangs)/betatmp;
-                fprintf('Computing analytical kernel... ')
-                model.hs=[60.0,60.0,180.0];
-                model.vp=[5.660,7.920,7.280];
-                model.vs=[3.2,4.4,4.05];
-                xs=(-600:1:150)';
-                zs=0:1:300;
-                [Kernel,X,Y,Z,KTimes,nTimes] = analytical_kernel_layered(rps, xs, zs, obj.tchar, obj.nderiv, model);
-                fprintf('done.\n')
-                Angles=incangs;
-
-            else
-               error('Bad Kernel_Type: %d',Kernel_Type);
-            end
-            obj.Kernel=Kernel;
-            obj.X=X;
-            obj.Y=Y;
-            obj.Z=Z;
-            obj.KTimes=KTimes;
-            obj.nTimes=nTimes;
-            obj.Angles=Angles;
+            obj.model.hs=[60.0,60.0,180.0];
+            obj.model.vp=[5.660,7.920,7.280];
+            obj.model.vs=[3.2,4.4,4.05];
+            obj.xs=(-600:10:150)';
+            obj.zs=0:10:300;
+            incangs=[20,23,26];
+            [~,betatmp]=get_velocity_from_profile('MIGRA/myvmod.nd',300);
+            obj.rps=sind(incangs)/betatmp;
+            obj.Angles=incangs;
         end
-    end      
-    
+        
+        function obj=load(obj)
+            if obj.Kernel_Type == 1;
+                path='/Volumes/nmancine/data2/nmancine/PROJECTS/SP_RECEIVER_FUNCTIONS/KERNEL/SP-KERNELS/DATA';
+                [obj.Kernel,obj.X,obj.Y,obj.Z,obj.KTimes,obj.nTimes,obj.Angles] = load_kernel(path,obj.tDeci);
+            elseif obj.Kernel_Type ==3;
+                fprintf('Computing analytical kernel... ')
+                [obj.Kernel,~,~,~,obj.KTimes,obj.nTimes] = analytical_kernel_layered(obj.rps, obj.xs, obj.zs, obj.tchar, obj.nderiv, obj.model);
+                fprintf('done.\n')
+            else
+               error('Bad Kernel_Type: %d',obj.Kernel_Type);
+            end
+            
+        end
+        
+        function [G,XSP]=get_amplitude_components(obj,Pdirect)
+            if obj.Kernel_Type~=3;
+                error('Bad Kernel_Type: %d',obj.Kernel_Type);
+            else
+                [~,~,Pscat,~,~]=read_in_necessary_files(Pdirect, obj.xs, obj.zs, obj.model);
+                [G] = geom_spreading(Pscat,obj.xs,obj.zs,obj.model);
+                [THE,~,~] = calc_angle(Pdirect,Pscat,obj.xs,obj.zs,obj.model);
+            %reference speeds 
+                vp=7.920;
+                vs=4.400;
+                rho=3.300;
+                vpert=0.01;
+                XSP = scattering_pattern(THE,vp,vs,rho,vpert);
+            end
+        end      
+    end
 end
