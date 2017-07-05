f3a21d9032a352a30aa765ddb76c0df41af0db4e
Nicholas Mancinelli <n.j.mancinelli@gmail.com>
2017-07-05 09:15:39 -0400

    Removed time2amp as a separate function.

--- MATLAB-CODE/FUNCTIONS/analytical_kernel_layered.m
+++ MATLAB-CODE/FUNCTIONS/analytical_kernel_layered.m
@@ -132,22 +132,3 @@ M12=Vpr;
 Krot = -M11 * Kz + M12 * Kr;
 
 end
-
-function [A] = time2amp(DT,trial_times,amps)
-
-%Simple gaussian 
-extrapval=0.0;
-
-A=interp1(trial_times,amps,DT,'pchip',extrapval);
-
-%A = exp(-DT.^2./(2.0*tchar^2));
-
-%Ricker wavelet
-%A = (1-(DT./tchar).^2) .* A;
-
-%Deriv of Ricker wavelet
-%A = -2*(tchar-DT)/tchar^2 .* A + (1-(DT/tchar).^2) .* (-DT/tchar^2) .* A;
-
-%A = -A .* (DT.^3 - 3*tchar^2 *DT)/tchar^4;
-
-end
