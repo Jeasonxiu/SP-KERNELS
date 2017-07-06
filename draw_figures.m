56d669f5fef867433b2b3451ec86d54c95c1a755
Nicholas Mancinelli <n.j.mancinelli@gmail.com>
2017-07-06 13:31:50 -0400

    Draw figures descriptions improved.

--- draw_figures.m
+++ draw_figures.m
@@ -4,35 +4,31 @@ addpath('MATLAB-CODE/FUNCTIONS')
 addpath('MATLAB-CODE/CLASSES')
 addpath('MIGRA')
 
-%% Sp Isochrons
+%% (1) Sp Isochrons
 f1=figure1();
 save(f1);
 
-%% Model Setup (use illustrator)
+%% (2) Model Setup (use illustrator)
 
-%% Numerical Kernels (show timing, angular dependence)
+%% (3) Numerical Kernels (show timing, angular dependence)
 f3=figure3;
 save(f3);
 
-%% Components of analytical kernels (Timing, Geometrical Spreading, Scattering Patterns)
+%% (4) Components of analytical kernels (Timing, Geometrical Spreading, Scattering Patterns)
 f4=figure4;
 save(f4);
 
-%% Benchmarking figure
+%% (5) Benchmarking figure
 f5=figure5;
 save(f5);
 
-%% Layered Kernels (Timing, Geometrical Spreading, Scattering Patterns)
-addpath('MATLAB-CODE/FUNCTIONS')
-plot_kernels_layered()
-
-%% Migration scheme
+%% (6) Migration scheme
 curdir=pwd;
 cd('OUTPUT/MIGRATION_FOR_KAREN')
 plot_model('model_1')
 cd(curdir)
 
-%% Inversion scheme
+%% (7) Inversion scheme
 curdir=pwd;
 cd('OUTPUT/INVERSION_FOR_KAREN')
 plot_model('model_1')
