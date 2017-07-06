d27e7b2d5474a5674362a33bf630076b23317523
Nicholas Mancinelli <n.j.mancinelli@gmail.com>
2017-07-06 14:45:31 -0400

    Defined properties and methods.

--- MATLAB-CODE/CLASSES/Inversion.m
+++ MATLAB-CODE/CLASSES/Inversion.m
@@ -1,9 +1,19 @@
 classdef Inversion
     properties
+        InversionParams=InversionParams();
+        velocity_model=velocity_model();
+        kernel=kernel(3);
+        %VelocityModel2D=VelocityModel2D();
     end
     methods
         function obj=Inversion()
         end
+        %function SetUpKernel(obj)
+        %end
+        %function SetUpMatrices(obj)
+        %end
+        %function RunInversion(obj)
+        %end
     end
     
 end
