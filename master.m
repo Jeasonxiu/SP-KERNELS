a4e0f86ac5c5cbc32ea98f31825f2a6895dfd2ce
Nicholas Mancinelli <n.j.mancinelli@gmail.com>
2017-07-06 13:31:01 -0400

    Link CLASSES directory

--- master.m
+++ master.m
@@ -1,13 +1,13 @@
 % A master script to perform Sp migration or inversion.
 % N. J. Mancinelli -- June 2017
 %
-runname=sprintf('OUTPUT/%s/SPACING_TEST_SNG',date);
+runname=sprintf('OUTPUT/%s/SPACING_TEST_DBL',date);
 
-InversionParams.skipSta=5;
-InversionParams.tDeci=5;
+InversionParams.skipSta=10;
+InversionParams.tDeci=20;
 InversionParams.dxin=5;
 InversionParams.dzin=2;
-InversionParams.Nus=[3.15E+1,3.15E+0,3.15E-1,3.15E-2];
+InversionParams.Nus=[3.15E+0,3.15E-1,3.15E-2];
 InversionParams.Norm_Opts=[2];
 InversionParams.nIterMax=500;
 InversionParams.direction=3; %3 (synthetics from both directions)
@@ -20,7 +20,7 @@ curdir=pwd;
 mkdir(runname)
 cd(runname)
 
-DirsToLink = {'MATLAB-CODE/FUNCTIONS','MIGRA','KERNEL-SEM/TEST_MODELS'};
+DirsToLink = {'MATLAB-CODE/FUNCTIONS', 'MATLAB-CODE/CLASSES', 'MIGRA', 'KERNEL-SEM/TEST_MODELS'};
 
 for Dir = DirsToLink;
     cmd=sprintf('ln -sf %s/%s .', curdir, Dir{1});
@@ -28,6 +28,7 @@ for Dir = DirsToLink;
 end
 
 addpath('FUNCTIONS');
+addpath('CLASSES')
 
 back_project_synthetics(InversionParams);
 
