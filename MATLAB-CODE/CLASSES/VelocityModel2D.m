fb7a7f953555e66c6baad5183d0c73162a15d6a9
Nicholas Mancinelli <n.j.mancinelli@gmail.com>
2017-07-06 14:47:36 -0400

    Added class for 2D VelocityModel

---
+++ MATLAB-CODE/CLASSES/VelocityModel2D.m
@@ -0,0 +1,14 @@
+classdef VelocityModel2D
+    properties
+        dlnvs
+        dlnvp
+        dlnrho
+        xs
+        zs
+    end
+    methods
+        function obj=VelocityModel2D()
+        end
+    end
+    
+end
