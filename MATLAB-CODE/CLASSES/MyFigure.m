5ac890aa4153afd5e2f6e753065a9551aa074244
Nicholas Mancinelli <n.j.mancinelli@gmail.com>
2017-09-20 11:32:19 -0400

    Save method added.

--- MATLAB-CODE/CLASSES/MyFigure.m
+++ MATLAB-CODE/CLASSES/MyFigure.m
@@ -8,5 +8,11 @@ classdef MyFigure
             set(obj.fig,'Units','centimeters')
             set(obj.fig,'Position',[0 0 widthCm heigthCm])
         end
+        function save(obj,filename)
+            obj.fig;
+            tmpstr=sprintf('%s.eps',filename);
+            print(tmpstr,'-depsc2', '-painters');
+            close;
+        end
     end
 end
